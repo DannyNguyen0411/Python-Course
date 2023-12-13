daaad85eb40888808a79f0ea71d8ce62a45c4793
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2023-12-13 16:21:17 +0100

    Day 24 Highscore

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -8,19 +8,27 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.hideturtle()
-        self.penup()
-        self.color("white")
         self.score = 0
+        self.high_score = 0
+        self.penup()
+        self.goto(0, 270)
+        self.color("white")
         self.update_score()
 
     def update_score(self):
         self.clear()
-        self.goto(0, 260)
         self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", False, align=ALIGNMENT, font=FONT)
+
     def increase_score(self, points):
         self.score += points
         self.update_score()
