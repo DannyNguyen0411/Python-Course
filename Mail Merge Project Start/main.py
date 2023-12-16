4fbf78c56a2fe5a4be5fa4f1dc1b60c93ed03b81
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2023-12-16 20:53:46 +0100

    Completed day 24

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,43 @@
+PLACEHOLDER = "[name]"
+
+#TODO: Create a letter using starting_letter.txt
+with open("input/Letters/starting_letter.txt") as letter:
+    invitation = letter.read()
+    print(invitation)
+
+#for each name in invited_names.txt
+with open("input/Names/invited_names.txt") as name:
+    names = name.readlines()
+    print(names)
+    for stripper in names:
+        stripped_name = stripper.strip()
+        # Replace the [name] placeholder with the actual name.
+        letter = invitation.replace(PLACEHOLDER, stripped_name)
+        print(f"{letter}")
+        with open(f"Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(letter)
+# #Save the letters in the folder "ReadyToSend".
+
+
+# # 👇Answer
+# PLACEHOLDER = "[name]"
+#
+# with open("./input/Names/invited_names.txt") as names_file:
+#     names = names_file.readlines()
+#
+# with open("./input/Letters/starting_letter.txt") as letter_file:
+#     letter_contents = letter_file.read()
+#     for name in names:
+#         stripped_name = name.strip()
+#         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+#         print(new_letter)
+#         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+#             completed_letter.write(new_letter)
+
+
+
+
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
--- day-24/main.py
+++ day-24/main.py
@@ -1,21 +1,21 @@
 # You have to use the close keyword so it will close the file.
-file = open("my_file.txt")
+file = open("/Users/danny/Desktop/my_file.txt")
 contenter = file.read()
 print(contenter)
 file.close()
 
-# Don't have to close it, because the key words will do it for you
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# # Don't have to close it, because the key words will do it for you
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
 
-# Edit text by using write by overwriting the file
-with open("new_file.txt", mode="w") as file:
-    file.write("New MotherFucking Text!")
+# # Edit text by using write by overwriting the file
+# with open("C:/Users/danny/Desktop/new_file.txt", mode="w") as file:
+#     file.write("New MotherFucking Text!")
 
-# Append text and write that in the file
-while True:
-    with open("new_file.txt", mode="a") as file:
-        file.write("\nNew MotherFucking Text!")
-
-# There are 3 modes: r, w and a
+# # Append text and write that in the file
+# while True:
+#     with open("new_file.txt", mode="a") as file:
+#         file.write("\nNew MotherFucking Text!")
+#
+# # There are 3 modes: r, w and a
