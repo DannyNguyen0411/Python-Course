4e8db361f1b7e82047cc20ab26c8e3bf344868b5
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2023-12-18 20:39:20 +0100

    Continued with day 25

--- day-25-Panda/main.py
+++ day-25-Panda/main.py
@@ -44,20 +44,55 @@ data = pandas.read_csv("weather_data.csv")
 # print(data["condition"])
 # print(data.condition)
 
-# Get Data in Row
-print(data[data.day == "Monday"])
-
-max_temperature = data[data.temp == data.temp.max()]
-print(max_temperature)
-
-monday = data[data.day == "Monday"]
-print(monday.condition)
-
-# # Warmest day of the week
-# max_temp = data["temp"].max()
+# # Get Data in Row
+# print(data[data.day == "Monday"])
 #
-# if max_temp == data["temp"].max:
-#     print(data["temp"].max)
+# max_temperature = data[data.temp == data.temp.max()]
+# print(max_temperature)
+
+
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp[0]
+# monday_fahrenheit = (monday_temp * 9 / 5) + 32
+# print(monday_fahrenheit)
+#
+# # Create a dataframe from scratch
+# data_dict = {
+#     "students" : ["Sonic", "Tails", "Knuckles"],
+#     "scores" : [70, 100, 50]
+# }
+#
+# data_students = pandas.DataFrame(data_dict)
+# data_students.to_csv("new_data.csv")
+
+data_squirrel = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20231218.csv")
+
+# # # Total squirrels 2968 in total.
+# fur_color = data_squirrel["Primary Fur Color"]
+# fur_color_counts = fur_color.value_counts()
+# print(fur_color_counts)
+
+grey_squirrels_count = len(data_squirrel[data_squirrel["Primary Fur Color"] == "Gray"])
+black_squirrels_count = len(data_squirrel[data_squirrel["Primary Fur Color"] == "Black"])
+cinnamon_squirrels_count = len(data_squirrel[data_squirrel["Primary Fur Color"] == "Cinnamon"])
+
+data_dict_squirrels = {
+    "Fur Color" : ["Gray", "Black", "Cinnamon"],
+    "Count" : [grey_squirrels_count, black_squirrels_count, cinnamon_squirrels_count]
+}
+
+data_info_squirrels = pandas.DataFrame(data_dict_squirrels)
+data_info_squirrels.to_csv("squirrel_count.csv")
+
+# fur_color_counts.to_csv("squirrel_count.csv")
+
+
+
+
+
+
+
+
 
 
 
