7b0f255d06155eef296ba32b5df33e250c16c452
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2024-02-01 19:50:04 +0100

    Capstone project finished

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -10,30 +10,22 @@ TIMER = None
 # --------------------------Read data-------------------------------------
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
-print(to_learn)
 
-french_words = []
-english_words = []
-language_words = []
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+    to_learn = data.to_dict(orient="records")
+except pandas.errors.EmptyDataError:
+    data = pandas.read_csv("data/french_words.csv")
+    to_learn = data.to_dict(orient="records")
+    print("File is empty! List is refilled.")
+except FileNotFoundError:
+    data_info_words = pandas.DataFrame(to_learn)
+    data_info_words.to_csv("data/words_to_learn.csv", index= False)
 
-for key in to_learn:
-    french_words.append(key["French"])
-    english_words.append(key["English"])
-print(french_words, english_words)
-current_card = {}
-
-# US map to see how to convert to "to_csv"
-
-data_dict_words_now = {
-    "French": french_words,
-    "English": english_words,
-}
-
-data_list_words_now = [f"{french_words}+{english_words}\n"]
 
 
 # --------------------------The function-------------------------------------
-def next_card():
+def remove_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
@@ -41,9 +33,10 @@ def next_card():
     canvas.itemconfig(text, text=current_card["French"], fill="black", font=FONT_BOLD)
     canvas.itemconfig(card_background, image=front_card_img)
     flip_timer = window.after(3000, func=flip_card)
+    to_learn.remove(current_card)
+    print(len(to_learn))
 
-    # data_dictator = data_list_words_now.remove("partie")
-    data_info_words = pandas.DataFrame(data_list_words_now)
+    data_info_words = pandas.DataFrame(to_learn)
     data_info_words.to_csv("data/words_to_learn.csv", index= False)
 
 
@@ -53,12 +46,14 @@ def flip_card():
     canvas.itemconfig(card_background, image=back_card_img)
 
 
-def wrong_card():
-    canvas.itemconfig(title, text="English", font=FONT_ITALIC)
-    canvas.itemconfig(text, text=current_card["French"], font=FONT_BOLD)
-    canvas.itemconfig(card_background, image=back_card_img)
-    data_info_words = pandas.DataFrame(data_dict_words_now)
-    data_info_words.to_csv("data/words_to_learn.csv")
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(title, text="French", fill="black", font=FONT_ITALIC)
+    canvas.itemconfig(text, text=current_card["French"], fill="black", font=FONT_BOLD)
+    canvas.itemconfig(card_background, image=front_card_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 # --------------------------UI Setup-------------------------------------
@@ -83,8 +78,8 @@ canvas.grid(column=0, row=0, columnspan=2)
 right_img = PhotoImage(file="images/right.png")
 wrong_img = PhotoImage(file="images/wrong.png")
 
-right_button = Button(image=right_img, highlightthickness=0, command=next_card)
-wrong_button = Button(image=wrong_img, highlightthickness=0, command=wrong_card)
+right_button = Button(image=right_img, highlightthickness=0, command=remove_card)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 wrong_button.grid(column=0, row=1)
 
